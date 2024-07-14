886bb7c370f8fef51d806a975a1c7249a97c3afb
carl louis manuel <carlxaeron09@gmail.com>
2024-07-15 01:01:04 +0800

    +

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,11 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap"
-        />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
       </head>
       <body className={inter.className}>
         <AppRouterCacheProvider>
