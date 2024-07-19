b004da90a40d55b046b2a7a0ddd8e8c82415dd00
Carl Louis Manuel <carl.manuel@metrobank.com.ph>
2024-07-19 15:06:01 +0800

    +

--- src/app/components/SanityRenderer.tsx
+++ src/app/components/SanityRenderer.tsx
@@ -1,13 +1,7 @@
 import React, { useContext } from "react";
-import Typography from '@mui/material/Typography';
 import Paper from '@mui/material/Paper';
 import { Context } from "../context/provider";
-
-const PTag = (props: any) => {
-  return (
-    <p>{props.children}</p>
-  )
-}
+import { PortableText } from "next-sanity";
 
 const ImgTag = (props: any) => {
   return (
@@ -22,13 +16,6 @@ export default function SanityRenderer(props: { data: any[]; }) {
   const ctx: any = useContext(Context);
 
   const component: { [key: string]: React.ComponentType<any> } = {
-    h1: Typography,
-    h2: Typography,
-    h3: Typography,
-    h4: Typography,
-    h5: Typography,
-    h6: Typography,
-    normal: PTag,
     media: ImgTag,
   }
 
@@ -60,18 +47,11 @@ export default function SanityRenderer(props: { data: any[]; }) {
     return props;
   }
 
-  const getText = (content: any) => {
-    if ((content?.children && content?.children.length > 0 && content?.children[0]?.text) || '') {
-      return (content?.children && content?.children.length > 0 && content?.children[0]?.text) || '';
-    }
-    else return '';
-  }
-
   console.log(props.data);
   return (
     <>
-      {props.data && props.data.map(content => (
-        React.createElement(component[getContent(content)], { key: content._key, ...getProps(content) }, getText(content))
+      {props.data && props.data.map(content => content._type == 'block' ? <PortableText value={content} key={content._key} /> : (
+        React.createElement(component[getContent(content)], { key: content._key, ...getProps(content) }, '')
       ))}
     </>
   )
