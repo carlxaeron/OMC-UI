9c3a7e1109547221147f92c4808eb315c11af6bb
carl louis manuel <carlxaeron09@gmail.com>
2024-07-30 00:28:39 +0800

    +

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -52,7 +52,7 @@ export default async function RootLayout({
                 style={{ fontFamily: "Public Sans, Noto Sans, sans-serif" }}>
                   <div className="layout-container flex h-full grow flex-col">
                     <Header />
-                    <div className="px-40 flex flex-1 justify-center py-5">
+                    <div className="@[480px]:px-40 flex flex-1 justify-center py-5">
                       <div className="layout-content-container flex flex-col max-w-[960px] flex-1">
                         {children}
                       </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,8 @@
 // import Container from '@mui/material/Container';
 // import { Metadata } from "next";
 
+import Hero from "./components/Hero";
+
 // export const metadata: Metadata = {
 //   title: "Home",
 //   description: "",
@@ -13,37 +15,7 @@
 export default function IndexPage() {
   return (
     <>
-      <div className="@container">
-        <div className="@[480px]:p-4">
-          <div
-            className="flex min-h-[480px] flex-col gap-6 bg-cover bg-center bg-no-repeat @[480px]:gap-8 @[480px]:rounded-xl items-center justify-center p-4"
-            style={{
-              backgroundImage:
-                'linear-gradient(rgba(0, 0, 0, 0.1) 0%, rgba(0, 0, 0, 0.4) 100%), url("https://cdn.usegalileo.ai/stability/f5a93fd6-eede-44fc-96c1-dde62d553329.png")',
-            }}
-          >
-            <div className="flex flex-col gap-2 text-center">
-              <h1 className="text-white text-4xl font-black leading-tight tracking-[-0.033em] @[480px]:text-5xl @[480px]:font-black @[480px]:leading-tight @[480px]:tracking-[-0.033em]">
-                {process.env.NEXT_PUBLIC_TITLE}
-              </h1>
-              <h2 className="text-white text-sm font-normal leading-normal @[480px]:text-base @[480px]:font-normal @[480px]:leading-normal">
-                {/* eslint-disable-next-line react/no-unescaped-entities */}
-                I'm passionate about helping people experience the love and
-                power of God. I do this through my work as an author, speaker,
-                and spiritual director.
-              </h2>
-            </div>
-            <div className="flex-wrap gap-3 flex justify-center">
-              <button className="flex min-w-[84px] max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-xl h-10 px-4 @[480px]:h-12 @[480px]:px-5 bg-[#1466b8] text-slate-50 text-sm font-bold leading-normal tracking-[0.015em] @[480px]:text-base @[480px]:font-bold @[480px]:leading-normal @[480px]:tracking-[0.015em]">
-                <span className="truncate">Donate</span>
-              </button>
-              <button className="flex min-w-[84px] max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-xl h-10 px-4 @[480px]:h-12 @[480px]:px-5 bg-[#e7edf3] text-[#0e141b] text-sm font-bold leading-normal tracking-[0.015em] @[480px]:text-base @[480px]:font-bold @[480px]:leading-normal @[480px]:tracking-[0.015em]">
-                <span className="truncate">Join</span>
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Hero />
       <div className="grid grid-cols-[repeat(auto-fit,minmax(158px,1fr))] gap-3 p-4">
         <div className="flex flex-col gap-3 pb-3">
           <div
