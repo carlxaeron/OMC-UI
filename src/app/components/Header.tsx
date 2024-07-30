92a6314f76a7be53ac8d852be8a4738279fba559
carl louis manuel <carlxaeron09@gmail.com>
2024-07-30 13:23:34 +0800

    +

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,17 +1,69 @@
 'use client';
 
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Context } from "../context/provider";
 import Link from 'next/link';
 import { usePathname, useRouter } from 'next/navigation';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes, faBars } from '@fortawesome/free-solid-svg-icons';
 
 export default function Header() {
   const ctx:any = useContext(Context);
   const pathName = usePathname();
   const router = useRouter();
 
+  const links = [
+    {title: 'Home', path: '/'},
+    {title: 'About', path: '/about'},
+    {title: 'Services', path: '/services'},
+    {title: 'Projects', path: '/projects'},
+    {title: 'Ministry', path: '/ministry'},
+    {title: 'Blog', path: '/blog'},
+  ];
+
+  const getLinks = () => {
+    return links.map((link) => {
+      return <Link onClick={() => setIsMobileNavOpen(false)} key={link.title} className={`${pathName === link.path ? 'underline ' : ''}text-[#111418] text-sm font-medium leading-normal`} href={link.path}>{link.title}</Link>
+    });
+  }
+
+  const [isMobileNavOpen, setIsMobileNavOpen] = useState(false);
+
+  const toggleMobileNav = () => {
+    setIsMobileNavOpen(!isMobileNavOpen);
+  };
+
+  const buttons = (data:any) => <>
+    <button className={`md:flex hidden min-w-[84px] max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-xl h-10 px-4 bg-[#1466b8] text-slate-50 text-sm font-bold leading-normal tracking-[0.015em]`}>
+      <span className="truncate">Donate</span>
+    </button>
+    <button
+      onClick={() => router.push('/signup/account')}
+      className={`md:flex hidden min-w-[84px] max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-xl h-10 px-4 bg-[#e7edf3] text-[#0e141b] text-sm font-bold leading-normal tracking-[0.015em]`}>
+      <span className="truncate">Join</span>
+    </button>
+    <button className={`md:flex hidden max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-xl h-10 bg-[#e7edf3] text-[#0e141b] gap-2 text-sm font-bold leading-normal tracking-[0.015em] min-w-0 px-2.5`}>
+      <div
+        className="text-[#0e141b]"
+        data-icon="MagnifyingGlass"
+        data-size="20px"
+        data-weight="regular"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="20px"
+          height="20px"
+          fill="currentColor"
+          viewBox="0 0 256 256"
+        >
+          <path d="M229.66,218.34l-50.07-50.06a88.11,88.11,0,1,0-11.31,11.31l50.06,50.07a8,8,0,0,0,11.32-11.32ZM40,112a72,72,0,1,1,72,72A72.08,72.08,0,0,1,40,112Z"></path>
+        </svg>
+      </div>
+    </button>
+  </>
+
   return (
-    <header className="flex items-center justify-between whitespace-nowrap border-b border-solid border-b-[#e7edf3] px-10 py-3">
+    <header className="fixed left-0 top-0 w-full bg-white z-50 flex items-center justify-between whitespace-nowrap border-b border-solid border-b-[#e7edf3] md:px-10 px-4 py-3">
       <div className="flex items-center gap-4 text-[#0e141b]">
         <div className="size-4">
           <svg
@@ -39,43 +91,25 @@ export default function Header() {
         </h2>
       </div>
       <div className="flex flex-1 justify-end gap-8">
-        <div className="flex items-center gap-9">
-          <Link href="/" className={`${pathName === '/' && 'underline'} text-[#0e141b] text-sm font-medium leading-normal`}>Home</Link>
-          <Link href="/about" className={`${pathName === '/about' && 'underline'} text-[#0e141b] text-sm font-medium leading-normal`}>About</Link>
-          <Link href="/services" className={`${pathName === '/services' && 'underline'} text-[#0e141b] text-sm font-medium leading-normal`}>Services</Link>
-          <Link href="/projects" className={`${pathName === '/projects' && 'underline'} text-[#0e141b] text-sm font-medium leading-normal`}>Projects</Link>
-          <Link href="/ministry" className={`${pathName === '/ministry' && 'underline'} text-[#0e141b] text-sm font-medium leading-normal`}>Ministry</Link>
-          <Link href="/blog" className={`${pathName === '/blog' && 'underline'} text-[#0e141b] text-sm font-medium leading-normal`}>Blog</Link>
+        <div className="md:flex items-center gap-9 hidden">
+          {getLinks()}
         </div>
         <div className="flex gap-2">
-          <button className="flex min-w-[84px] max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-xl h-10 px-4 bg-[#1466b8] text-slate-50 text-sm font-bold leading-normal tracking-[0.015em]">
-            <span className="truncate">Donate</span>
-          </button>
+          {buttons({ desktop: true })}
           <button
-            onClick={() => router.push('/signup/account')}
-            className="flex min-w-[84px] max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-xl h-10 px-4 bg-[#e7edf3] text-[#0e141b] text-sm font-bold leading-normal tracking-[0.015em]">
-            <span className="truncate">Join</span>
-          </button>
-          <button className="flex max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-xl h-10 bg-[#e7edf3] text-[#0e141b] gap-2 text-sm font-bold leading-normal tracking-[0.015em] min-w-0 px-2.5">
-            <div
-              className="text-[#0e141b]"
-              data-icon="MagnifyingGlass"
-              data-size="20px"
-              data-weight="regular"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="20px"
-                height="20px"
-                fill="currentColor"
-                viewBox="0 0 256 256"
-              >
-                <path d="M229.66,218.34l-50.07-50.06a88.11,88.11,0,1,0-11.31,11.31l50.06,50.07a8,8,0,0,0,11.32-11.32ZM40,112a72,72,0,1,1,72,72A72.08,72.08,0,0,1,40,112Z"></path>
-              </svg>
-            </div>
+            className="md:hidden flex items-center justify-center w-10 h-10 bg-[#f0f2f4] text-[#111418] rounded-md"
+            onClick={toggleMobileNav}
+          >
+            <FontAwesomeIcon icon={isMobileNavOpen ? faTimes : faBars} width="20" height="20" />
           </button>
         </div>
       </div>
+      {isMobileNavOpen && (
+        <div className="top-[100%] w-full z-50 bg-white absolute left-0 md:hidden flex flex-col gap-4 py-2 items-end pr-4">
+          {getLinks()}
+          {buttons({ desktop: false })}
+        </div>
+      )}
     </header>
   )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -50,7 +50,7 @@ export default async function RootLayout({
               {/* <ThirdwebProvider> */}
                 <main className="relative flex size-full min-h-screen flex-col bg-slate-50 group/design-root overflow-x-hidden"
                 style={{ fontFamily: "Public Sans, Noto Sans, sans-serif" }}>
-                  <div className="layout-container flex h-full grow flex-col">
+                  <div className="layout-container flex h-full grow flex-col md:pt-[65px] pt-[45px]">
                     <Header />
                     <div className="@[480px]:px-40 flex flex-1 justify-center py-5">
                       <div className="layout-content-container flex flex-col max-w-[960px] flex-1">
