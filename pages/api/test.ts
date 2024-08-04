99a29d49dfede5adf254c5f897c8280ac16fb549
carl louis manuel <carlxaeron09@gmail.com>
2024-08-04 16:48:00 +0800

    +

--- pages/api/test.ts
+++ pages/api/test.ts
@@ -15,8 +15,8 @@ async function scrapeData(props:{
     const loginUrl = 'https://apics.partnerrc.com/am/@@login'; // Login URL
     const exploreUrl = 'https://apics.partnerrc.com/cscp2023/explore'; // Replace with the URL to scrape
 
-    // const browser = await puppeteer.launch({ headless: false, args: ['--no-sandbox', '--disable-setuid-sandbox'] });
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({ headless: false, args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+    // const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
     await page.setJavaScriptEnabled(true);
@@ -88,17 +88,115 @@ async function scrapeData(props:{
 
     if(props.categoryId) await page.locator(`${gscnPSelector}[class*="_3bMNCRqJ"]`).waitHandle();
     
-    const quizSelector = await page.locator('[xqa="accordion"]').waitHandle();
-    const quiz = await quizSelector?.evaluate(el =>
-      Array.from(el.querySelectorAll('article')).map((article: any) => ({
-        title: article.querySelector('._2-BcwAqe + div > span:nth-child(1)')?.innerText,
-        description: article.querySelector('[xqa="shortdesc"] p')?.innerText,
-      }))
-    )
+    let quiz: any[] = [];
+    let data: any[] = [];
+    const quizzesSeletor = `[xqa="accordion"]`;
+    if(props.quizId) {
+      const quizzesData = await new Promise<any[]>((resolve, reject) => {
+        db.all("SELECT * FROM quiz where category_id = ?", props.categoryId || 1, (err, rows) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(rows);
+          }
+        });
+      });
+      const selectedQuiz = quizzesData.filter((quiz: any, quizI) => quizI === props.quizId)[0];
+      console.log(selectedQuiz);
 
-    const data: { title: string; description: string; }[] = [];
-    for (const { title, description } of quiz) {
-      data.push({ title, description });
+      if(props.quizId !== 1) {
+        const quizS = await page.locator(`${quizzesSeletor} article:nth-child(${props.quizId})`).waitHandle()
+        await quizS.click();
+      }
+      const quizSelector = await page.locator(`${quizzesSeletor} article:nth-child(${props.quizId}) section [xqa="quiz-action-button"] > *:last-child`).waitHandle();
+      await quizSelector.scrollIntoView();
+      await quizSelector.click();
+      const quizPageSelector = await page.locator('[xqa="progression question-feedback"]').waitHandle();
+      
+      // Check if the quiz has a begin button
+      let quizPageType: any = {};
+      try {
+        await Promise.race([
+          page.locator('[xqa="progression question-feedback"] [xqa="action begin"]').waitHandle(),
+          new Promise((resolve) => setTimeout(resolve, 10000))
+        ]);
+        const hasBegin = await quizPageSelector?.evaluate((el: Element) => {
+          return el.querySelector('[xqa="action begin"]') ? true : false
+        });
+        quizPageType = {...quizPageType, hasBegin};
+      } catch (error) {
+        console.log('Page locator not found:', error);
+      }
+      // Check if the quiz has a restart button
+      if(!quizPageType.hasBegin) {
+        try {
+          await Promise.race([
+            page.locator('[xqa="progression question-feedback"] [xqa="action begin"]').waitHandle(),
+            new Promise((resolve) => setTimeout(resolve, 10000))
+          ]);
+          const hasRestart = await quizPageSelector?.evaluate((el: Element) => {
+            return el.querySelector('[xqa="action restart"]') ? true : false
+          });
+          quizPageType = {...quizPageType, hasRestart, hasBegin: false};
+        } catch (error) {
+          console.log('Page locator not found:', error);
+        }
+      }
+      
+      if(quizPageType.hasBegin) {
+        await page.locator('[xqa="action begin"] + button').click();
+        let isBtnDisabled = false;
+        const next = '[xqa="action submit"] > *:nth-child(2) >';
+        const nextBtn = next + ' button:not([disabled])';
+        const nextBtnDisabled = next + ' button[disabled]';
+        try {
+          await Promise.race([
+            page.locator(nextBtn).waitHandle(),
+            new Promise((resolve) => setTimeout(resolve, 10000))
+          ]);
+        } catch (error) {
+          console.log('Page locator not found:', error);
+        }
+        const questionsData = [];
+        const maxQuestions = 5;
+        let questionPageCnt = 0;
+        while(isBtnDisabled === false && questionPageCnt <= maxQuestions) {
+          const questionSelector = await page.locator('[xqa*="MCQuestion"]').waitHandle();
+          const d = await questionSelector?.evaluate((el: Element) => {
+            let answers:any = [];
+            el.querySelectorAll('fieldset label').forEach((label: Element) => {
+              const text = label?.textContent?.replaceAll('CorrectCorrect', '').replaceAll('IncorrectIncorrect', '') || '';
+              answers.push({ text, isCorrect: label?.textContent?.includes('Correct') });
+            });
+
+            return {
+              rationale: el.querySelector('div:nth-child(1) > h2 + div')?.textContent,
+              question: el.querySelector('._2sv26PNy > div > p')?.textContent,
+              answers,
+            }
+          });
+          questionsData.push(d);
+          await page.locator(nextBtn).click();
+          questionPageCnt++;
+        }
+        data = questionsData;
+      }
+      if(quizPageType.hasRestart) {
+        await page.locator('[xqa="action restart"]').click();
+      }
+    } else {
+      const quizSelector = await page.locator(`${quizzesSeletor}`).waitHandle();
+      quiz = await quizSelector?.evaluate(el =>
+        Array.from(el.querySelectorAll('article')).map((article: any) => ({
+          title: article.querySelector('._2-BcwAqe + div > span:nth-child(1)')?.innerText,
+          description: article.querySelector('[xqa="shortdesc"] p')?.innerText,
+        }))
+      )
+  
+      const data: { title: string; description: string; }[] = [];
+      for (const { title, description } of quiz) {
+        data.push({ title, description });
+      }
     }
 
     // Save data to the database
