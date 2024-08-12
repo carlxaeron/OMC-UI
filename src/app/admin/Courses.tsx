eec12573690ae582c939efe1b45805df3e433ca3
carl louis manuel <>
2024-08-12 23:32:59 +0800

    +

--- src/app/admin/Courses.tsx
+++ src/app/admin/Courses.tsx
@@ -7,7 +7,7 @@ export default function Courses() {
   return (
     <>
       <PageHeader title="Courses" />
-      <div className="flex gap-4">
+      <div className="md:flex gap-4">
         {[...Array(3)].map((_, index) => (
           <CourseCard key={index} />
         ))}
--- src/app/components/admin/CourseCard.tsx
+++ src/app/components/admin/CourseCard.tsx
@@ -2,7 +2,7 @@ import { Card, CardBody, Chip } from "@material-tailwind/react";
 
 export default function CourseCard(props: any = {}) {
   return (
-    <Card className="w-1/3">
+    <Card className="md:w-1/3 mb-4 md:mb-0">
       <CardBody className="text-black">
         <Chip value="Free" size="sm" className="bg-gray-200 text-black inline" />
         <font className='block text-black my-3'>Beginner’s Guide to Design</font>
